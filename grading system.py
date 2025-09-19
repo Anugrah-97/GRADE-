f2b785d20d50e5b3a2a5bbbf257871d55b26d313
Anugrah-97 <vargheseanugrah7d@gmail.com>
2025-09-19 14:10:35 +0530

    Add files via upload

---
+++ grading system.py
@@ -0,0 +1,13 @@
+TO MAKE MARKS ANALYSER
+
+marks=int(input("your marks:-"))
+if marks>=80:
+    print("your grade A")
+elif marks >=70:
+    print("your grade B")
+elif marks >=60:
+    print("your grade C")
+elif marks >=50:
+    print("your grade D")
+else:
+    print("grade is c")
